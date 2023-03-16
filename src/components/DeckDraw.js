da0c76d102dd4fed6ff265801d382f1ce382407f
Lorenzo <lorenzopalumbo.dev@gmail.com>
2023-03-16 17:47:26 +0100

    commit

--- src/components/DeckDraw.js
+++ src/components/DeckDraw.js
@@ -32,6 +32,7 @@ const DeckDraw = () => {
     if (playerInfo.score > 7.5) {
       setGameSettings({...gameSettings, canDraw:false});
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [playerInfo.score]);
 
   const draw = (deckAfterFirstDraw) => {
